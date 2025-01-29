f3314357a3f9f566049e4abec947fc219060751a
Gabi <gongoragabrielvalle@gmail.com>
2025-01-29 10:14:21 +0100

    docu 301

---
+++ Desplegament/2a av/Recuss/acts/0301/projecte_joomla/www/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-01-28 19:24:45 UTC
+#Software: Joomla! 5.2.3 Stable [ Uthabiti ] 7-January-2025 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-01-28T19:24:45+00:00	INFO 172.23.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
