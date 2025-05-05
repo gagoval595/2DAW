d93d2eda79a98395a58ee3519fbeb73039c3f659
gabi <gongoragabrielvalle@gmail.com>
2025-05-05 23:54:31 +0200

    vale tete header i mapa fet

--- RallyGO/Front/rallygo/src/App.js
+++ RallyGO/Front/rallygo/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MapView from "./components/MapView";
 import "leaflet/dist/leaflet.css"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Mapa de RallyGO</h1>
       <MapView />
     </div>
---
+++ RallyGO/Front/rallygo/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from "../img/LogoRallyGO.png";
+import "../App.css";
+
+function Header() {
+  return (
+    <Navbar expand="sm" className="navbar-light p-0 border-bottom border-dark border-1">
+      <Container className="d-flex justify-content-center ">
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="my-lg-0 d-flex align-items-center" navbarScroll>
+            <img src={logo} className="logo d-md-none d-sm-block" alt="Logo RallyGO" />
+
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/equipos">Calendario</Nav.Link>
+            <Nav.Link href="/equipos">Equipos</Nav.Link>
+
+            <Navbar.Brand href="/home">
+              <img src={logo} className="logo d-none d-md-block" alt="Logo RallyGO" />
+            </Navbar.Brand>
+
+            <Nav.Link href="/galeria">Galeria</Nav.Link>
+            <Nav.Link href="/servicios">Servicios</Nav.Link>
+            <Nav.Link href="/contacto">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
